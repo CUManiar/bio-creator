b59f86162e737700b7c225eca3d5b8c9bdc7f345
cumaniar <maniarchirag57@gmail.com>
2020-03-09 19:41:00 +0530

    Updates

---
+++ precache-manifest.f0bc04f0201b4f486120f2147204be98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451b198ea4c17960eb0dbd2555df7357",
+    "url": "/bio-creator/index.html"
+  },
+  {
+    "revision": "c1e4d478106947e50789",
+    "url": "/bio-creator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5ac2a20a767848934c43",
+    "url": "/bio-creator/static/css/main.4b18cf07.chunk.css"
+  },
+  {
+    "revision": "c1e4d478106947e50789",
+    "url": "/bio-creator/static/js/2.90c73052.chunk.js"
+  },
+  {
+    "revision": "85d5e4cf7c12c1b157d668da456ca566",
+    "url": "/bio-creator/static/js/2.90c73052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac2a20a767848934c43",
+    "url": "/bio-creator/static/js/main.198a770d.chunk.js"
+  },
+  {
+    "revision": "2743b2c1b157c27e5364",
+    "url": "/bio-creator/static/js/runtime-main.00c1dda9.js"
+  }
+]);
